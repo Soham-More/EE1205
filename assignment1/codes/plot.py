f039007e05ebbac256e46a12948ca884daeabcba
Soham <msoham2100@gmail.com>
2024-01-25 21:13:59 +0530

    Removed function

--- assignment1/codes/plot.py
+++ assignment1/codes/plot.py
@@ -9,13 +9,10 @@ N = 13
 # and cast to float32 type
 seq = (np.arange(-4, N, 1)).astype(np.float32)
 
-# construct a GP sequence
-def getGPsequence(a, r):
-    GP = a * np.power(r, seq) * np.heaviside(seq, 1)
-    return GP
-
 # a_n GP
-plt.stem(seq, getGPsequence(2, 2.0 ** 0.5), 'ro', basefmt='k')
+GP_A = 2 * np.power(2.0 ** 0.5, seq) * np.heaviside(seq, 1)
+
+plt.stem(seq, GP_A, 'ro', basefmt='k')
 plt.stem([12], [128], 'mo', basefmt='k', label='$x_1(k_1)$')
 plt.xlabel('n')
 plt.ylabel('$x_1$(n)')
@@ -26,8 +23,11 @@ plt.savefig('figs/a.png')
 
 # clear previous plot
 plt.clf()
+
 # b_n GP
-plt.stem(seq, getGPsequence(3.0 ** 0.5, 3.0 ** 0.5), 'go', basefmt='k')
+GP_B = 3.0 ** 0.5 * np.power(3.0 ** 0.5, seq) * np.heaviside(seq, 1)
+
+plt.stem(seq, GP_B, 'go', basefmt='k')
 plt.stem([11], [729], 'mo', basefmt='k', label='$x_2(k_2)$')
 plt.xlabel('n')
 plt.ylabel('$x_2$(n)')
@@ -38,8 +38,11 @@ plt.savefig('figs/b.png')
 
 # clear previous plot
 plt.clf()
-# c_n GP
-plt.stem(seq, getGPsequence(1/3, 1/3), 'bo', basefmt='k')
+
+# b_n GP
+GP_C = np.power(1 / 3.0, seq) * np.heaviside(seq, 1) / 3.0
+
+plt.stem(seq, GP_C, 'bo', basefmt='k')
 plt.stem([8], [1/19683], 'mo', basefmt='k', label='$x_3(k_3)$')
 plt.xlabel('n')
 plt.ylabel('$x_3$(n)')
