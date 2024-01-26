68711489ddb794b67b1735e890daf265861ef5a0
Soham <msoham2100@gmail.com>
2024-01-26 14:26:07 +0530

    Using C to generate sequence

--- assignment1/codes/plot.py
+++ assignment1/codes/plot.py
@@ -1,18 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
-# only odd N
-# even N non-symmetric output
-N = 13
-
-# make a sequence of numbers from -N//2 to N//2 (for odd N)
-# and cast to float32 type
-seq = (np.arange(-4, N, 1)).astype(np.float32)
+values = np.loadtxt('codes/values.txt', delimiter=' ')
 
 # a_n GP
-GP_A = 2 * np.power(2.0 ** 0.5, seq) * np.heaviside(seq, 1)
-
-plt.stem(seq, GP_A, 'ro', basefmt='k')
+plt.stem(values[0], values[1], 'ro', basefmt='k')
 plt.stem([12], [128], 'mo', basefmt='k', label='$x_1(k_1)$')
 plt.xlabel('n')
 plt.ylabel('$x_1$(n)')
@@ -25,9 +17,7 @@ plt.savefig('figs/a.png')
 plt.clf()
 
 # b_n GP
-GP_B = 3.0 ** 0.5 * np.power(3.0 ** 0.5, seq) * np.heaviside(seq, 1)
-
-plt.stem(seq, GP_B, 'go', basefmt='k')
+plt.stem(values[0], values[2], 'go', basefmt='k')
 plt.stem([11], [729], 'mo', basefmt='k', label='$x_2(k_2)$')
 plt.xlabel('n')
 plt.ylabel('$x_2$(n)')
@@ -40,9 +30,7 @@ plt.savefig('figs/b.png')
 plt.clf()
 
 # b_n GP
-GP_C = np.power(1 / 3.0, seq) * np.heaviside(seq, 1) / 3.0
-
-plt.stem(seq, GP_C, 'bo', basefmt='k')
+plt.stem(values[0], values[3], 'bo', basefmt='k')
 plt.stem([8], [1/19683], 'mo', basefmt='k', label='$x_3(k_3)$')
 plt.xlabel('n')
 plt.ylabel('$x_3$(n)')
