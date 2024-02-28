be0293a045f13fddac88652cc6837a9b2ef4bc23
root <root@localhost.localdomain>
2024-02-28 14:13:57 +0000

    changed w to omega

--- assignment4/codes/plot.py
+++ assignment4/codes/plot.py
@@ -6,7 +6,7 @@ values = np.loadtxt('codes/values.txt', delimiter=' ')
 # a_n GP
 plt.plot(values[0], values[1], 'r')
 plt.stem([37.5, 62.5, 137.5, 162.5], [3] * 4, 'r', basefmt=' ')
-plt.xlabel('$w$')
+plt.xlabel('$\omega$')
 plt.ylabel('$\\left\\vert{X_s(j\omega)}\\right\\vert$')
 plt.grid()
 # save plot
